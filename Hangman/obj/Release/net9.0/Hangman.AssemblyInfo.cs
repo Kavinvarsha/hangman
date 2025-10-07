80e8c5fdc1133e5b37816fbdd3c75e96fd1f2435
varshaa <kavinvarsha33@gmail.com>
2025-10-07 09:07:27 +0530

    model view controller

--- Hangman/obj/Release/net9.0/Hangman.AssemblyInfo.cs
+++ Hangman/obj/Release/net9.0/Hangman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939d212f8c2167adfe543dd8fe7cacb35782c577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6108fe97da72182b30bb32f4889ac5361d385f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
